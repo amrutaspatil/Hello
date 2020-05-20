c013b0c1e74e6b8d47acd30ea13c331bdccb59c2
amrutaspatil <amruta.3.patil@atos.net>
2020-05-20 17:58:56 +0530

    commit second final

---
+++ Desktop/STG Cloud/Eclipse/SparseTest2/src/main/java/Demo/Sparse/SparseTest2/App.java
@@ -0,0 +1,13 @@
+package Demo.Sparse.SparseTest2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Amruta !" );
+    }
+}
